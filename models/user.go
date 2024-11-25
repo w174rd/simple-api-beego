b82f526450129f7235383314064e8a139b7aaf17
najib <najibbudinabdullah@gmail.com>
2024-11-25 08:22:59 +0700

    update user

--- models/user.go
+++ models/user.go
@@ -8,8 +8,8 @@ type User struct {
 	Id        int        `json:"id" orm:"auto"`
 	Name      string     `json:"name" orm:"size(100)"`
 	Email     string     `json:"email" orm:"size(100)"`
-	Password  string     `json:"password,omitempty" orm:"null;size(100)"`
-	Token     string     `json:"token,omitempty"`
+	Password  string     `json:"password,omitempty" orm:"null"`
+	Token     string     `json:"token,omitempty" orm:"-"`
 	DeletedAt *time.Time `json:"deleted_at,omitempty" orm:"null;type(timestamp)"`
 	CreatedAt *time.Time `json:"created_at,omitempty" orm:"auto_now_add;type(timestamp)"`
 	UpdatedAt *time.Time `json:"updated_at,omitempty" orm:"auto_now;type(timestamp)"`
